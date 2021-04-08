2f9d7e323440452218dd02391ee0d728bcb7cec3
pventrella20 <p.ventrella4@studenti.uniba.it>
2021-04-08 18:50:57 +0200

    - ultimi fix

--- dataset/__init__.py
+++ dataset/__init__.py
@@ -7,5 +7,5 @@ __version__ = '0.1'
 __author__ = 'Vito Walter Anelli, Claudio Pomo'
 __email__ = 'vitowalter.anelli@poliba.it, claudio.pomo@poliba.it'
 
-from elliot.dataset.dataloader.knowledge_aware_chain import KnowledgeChainsLoader
+from elliot.dataset.dataloader.knowledge_aware_chains import KnowledgeChainsLoader
 from elliot.dataset.dataloader.visual_loader import VisualLoader
