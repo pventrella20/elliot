960be2b52043c42ab848752872f273b2d1b10783
pventrella20 <p.ventrella4@studenti.uniba.it>
2021-04-09 00:24:35 +0200

    - ultimi fix

--- recommender/test_item_strategy.py
+++ recommender/test_item_strategy.py
@@ -1,23 +1,16 @@
-def test_item_only_filter(recomm_list, test_list):
+def only_test_item_filter(recomm_list, test_list):
     filtered = {}
     test_item_list = {}
-    rec_item_list = {}
     for u, i_s in test_list.items():
         i_l = []
         for item in i_s:
             i_l.append(item)
         test_item_list[u] = i_l
     for u, i_s in recomm_list.items():
-        i_l = []
-        for item in i_s:
-            i_l.append(item[0])
-        rec_item_list[u] = i_l
-    for u, i_s in recomm_list.items():
         new_rec_list = []
-        for item in rec_item_list[u]:
-            for tup in i_s:
-                if item in test_item_list[u] and item == tup[0]:
-                    new_rec_list.append((item, tup[1]))
+        for tup in i_s:
+            if tup[0] in test_item_list[u]:
+                    new_rec_list.append(tup)
         filtered[u] = new_rec_list
 
     return filtered
